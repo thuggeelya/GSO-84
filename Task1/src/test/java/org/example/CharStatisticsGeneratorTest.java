4b9dfea3dd5931bb0bbe526147771e927fea1a86
thuggeelya <ilya-fdmdh@mail.ru>
2022-05-25 12:00:09 +0700

    temp commit

--- Task1/src/test/java/org/example/CharStatisticsGeneratorTest.java
+++ Task1/src/test/java/org/example/CharStatisticsGeneratorTest.java
@@ -13,14 +13,16 @@ import static org.junit.Assert.assertTrue;
 public class CharStatisticsGeneratorTest
 {
     @Before
-    public void generate() {
+    public void generate()
+    {
         String inp = "src/test/resources/input.txt";
         CharStatisticsGenerator.generate(inp, "src/test/resources/output1.txt");
         CharStatisticsGenerator.generate(inp, "src/test/resources/output2.txt");
     }
 
     @Test
-    public void charStatisticsGenerator() throws IOException {
+    public void charStatisticsGenerator() throws IOException
+    {
         Path out1 = Path.of("src/test/resources/output1.txt");
         Path out2 = Path.of("src/test/resources/output2.txt");
         assertTrue(Files.exists(out1));
