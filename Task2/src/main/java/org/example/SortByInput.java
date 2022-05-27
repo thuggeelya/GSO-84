2b10eb93aa49bfc498e92d4d0a6518f7781547e7
thuggeelya <ilya-fdmdh@mail.ru>
2022-05-27 13:17:13 +0700

    code review #3

--- Task2/src/main/java/org/example/SortByInput.java
+++ Task2/src/main/java/org/example/SortByInput.java
@@ -8,20 +8,42 @@ import java.io.InputStreamReader;
 import java.lang.reflect.InvocationTargetException;
 import java.util.logging.Logger;
 
+/**
+ * Sorts the text using sorting strategies.
+ * @see org.sort.IFileSortingStrategy
+ */
 public class SortByInput {
 
-    public static void sort(String fileName, String outputFileName, String fullyQualifiedClass) {
+    /**
+     * Sorts the text given in {@code inputFile} according to the
+     * sorting strategy, defined in {@code fullyQualifiedClass} implements
+     * {@link org.sort.IFileSortingStrategy}, and writes it into {@code outputFile}.
+     * @param inputFile Content path to input file
+     * @param outputFile Content path to output file
+     * @param fullyQualifiedClass Sorting class canonical name
+     * @throws ClassNotFoundException if {@code fullyQualifiedClass} is missing
+     * @see #sort(String fullyQualifiedClass)
+     */
+    public static void sort(String inputFile, String outputFile, String fullyQualifiedClass) throws ClassNotFoundException {
         try {
             Class<?> c = Class.forName(fullyQualifiedClass);
             IFileSortingStrategy instance = (IFileSortingStrategy) c.getDeclaredConstructor().newInstance();
-            instance.sort(instance.readFile(fileName), outputFileName);
-        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | NoSuchMethodException |
+            instance.sort(instance.readFile(inputFile), outputFile);
+        } catch (InstantiationException | IllegalAccessException | NoSuchMethodException |
                  InvocationTargetException e) {
             Logger.getGlobal().severe("Something went wrong: " + e);
         }
     }
 
-    public static void sort(String fullyQualifiedClass) {
+    /**
+     * Sorts text given in file according to the
+     * sorting strategy, defined in {@code fullyQualifiedClass} implements
+     * {@link org.sort.IFileSortingStrategy}, and writes it into another file.
+     * File paths need to be given from terminal.
+     * @param fullyQualifiedClass Sorting class canonical name
+     * @throws ClassNotFoundException if {@code fullyQualifiedClass} is missing
+     */
+    public static void sort(String fullyQualifiedClass) throws ClassNotFoundException {
         try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
             System.out.println("Input file:");
             String inputFile = br.readLine();
--- Task2/src/main/java/org/sort/IFileSortingStrategy.java
+++ Task2/src/main/java/org/sort/IFileSortingStrategy.java
@@ -5,18 +5,46 @@ import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
+/**
+ * Strategy to sort text in <i>files</i>.
+ *
+ * The {@code IFileSortingStrategy} interface provides two methods for text sorting.
+ * The user can persist the output path in signature or using terminal.
+ */
 public interface IFileSortingStrategy extends FileWritable, FileReadable{
 
+    /**
+     * @return <i>sorting strategy</i> as a {@code Comparator}
+     */
     Comparator<String> getSortingStrategy();
 
+    /**
+     * Sorts the words of input text
+     * and writes the result into the <i>file</i>.
+     *
+     * File path needs to be given from terminal.
+     * @param text A {@code String} array consisting of input text words
+     */
     default void sort(String[] text) {
         writeFile(getLines(text));
     }
 
+    /**
+     * Sorts the words of input text
+     * and writes the result into the <i>file</i>.
+     *
+     * @param text A {@code String} array consisting of input text words
+     * @param outputFileName An output file content path
+     */
     default void sort(String[] text, String outputFileName) {
         writeFile(outputFileName, getLines(text));
     }
 
+    /**
+     * @param text An array containing words from read file
+     * @return List sorted by <i>sorting strategy</i>.
+     * @see #getSortingStrategy()
+     */
     default List<String> getLines(String[] text) {
         return Arrays.stream(text)
                 .sorted(getSortingStrategy())
