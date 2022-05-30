c9cf7235bfea7d63f9e67f903ca049ccbf6db716
thuggeelya <ilya-fdmdh@mail.ru>
2022-05-30 11:56:46 +0700

    code review #8

--- Task1/src/test/java/org/example/RealCollector.java
+++ Task1/src/test/java/org/example/RealCollector.java
@@ -6,6 +6,9 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.atomic.AtomicLong;
 
+/**
+ * Contains known in advance result according to the input file content.
+ */
 public final class RealCollector {
 
     @SuppressWarnings("unused")
