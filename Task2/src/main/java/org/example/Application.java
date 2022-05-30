41c3b4da66bea5fddbcf2a5c59b7564c10f0261f
thuggeelya <ilya-fdmdh@mail.ru>
2022-05-30 11:48:27 +0700

    code review #8

--- Task2/src/main/java/org/example/Application.java
+++ Task2/src/main/java/org/example/Application.java
@@ -7,10 +7,12 @@ import static org.example.SortByInput.sort;
 public class Application {
 
     public static void main(String[] args) throws ClassNotFoundException {
-        sort("Task2/src/main/resources/input.txt", "Task2/src/main/resources/SortLength.txt", SortLength.class.getCanonicalName());
-        sort("Task2/src/main/resources/input.txt", "Task2/src/main/resources/SortAlphabetic.txt", SortAlphabetic.class.getCanonicalName());
-        sort("Task2/src/main/resources/input.txt", "Task2/src/main/resources/SortAlphabeticSorted.txt", SortAlphabeticSorted.class.getCanonicalName());
-        sort("Task2/src/main/resources/input.txt", "Task2/src/main/resources/SortAlphabeticTurned.txt", SortAlphabeticTurned.class.getCanonicalName());
-        sort("Task2/src/main/resources/input.txt", "Task2/src/main/resources/SortFirstLetterCodeDigitsSum.txt", SortFirstLetterCodeDigitsSum.class.getCanonicalName());
+        sort(SortLength.class.getCanonicalName());
+        sort(SortAlphabetic.class.getCanonicalName());
+        sort(SortAlphabeticSorted.class.getCanonicalName());
+        sort(SortAlphabeticTurned.class.getCanonicalName());
+        sort("Task2/src/main/resources/input.txt",
+                "Task2/src/main/resources/SortFirstLetterCodeDigitsSum.txt",
+                SortFirstLetterCodeDigitsSum.class.getCanonicalName());
     }
 }
