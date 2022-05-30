3397706dbcfe302a8284e6d836cf21624e782e4d
thuggeelya <ilya-fdmdh@mail.ru>
2022-05-30 11:47:18 +0700

    code review #8

---
+++ Task1/src/main/java/org/example/Application.java
@@ -0,0 +1,10 @@
+package org.example;
+
+import org.statistics.CharStatisticsGenerator;
+
+public class Application {
+
+    public static void main(String[] args) {
+        new CharStatisticsGenerator().generate();
+    }
+}
